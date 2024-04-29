44ebe4550bb9020ecf05bb8a6ecea773e0eee509
nipsalvin <amwaniki.am@gmail.com>
2024-04-29 07:36:14 +0300

    `section 3` initial commit

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('january', views.index)
+    # path('january/', views.january),
+    # path('february/', view=views.february)
+    path('<month>/', view=views.monthly_challenges)
 ]
+
