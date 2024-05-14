ce34c397dcc30dcf1ac08cce1b4b9b8c677b43b5
nipsalvin <amwaniki.am@gmail.com>
2024-05-14 07:46:15 +0300

    Adding css styling

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request=request, template_name='blog/index.html')
 
 def all_posts(request):
-    pass
+    return render(request=request, template_name='blog/all_posts.html')
 
 def post(request):
     pass
