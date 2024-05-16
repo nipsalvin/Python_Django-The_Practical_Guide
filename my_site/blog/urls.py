a91df198069019a867ed3dc134982d700525987f
nipsalvin <amwaniki.am@gmail.com>
2024-05-16 19:00:52 +0300

    adding `single-post` page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -5,5 +5,5 @@ from blog.views import *
 urlpatterns = [
     path('', view=index, name='index'),
     path('posts', view=all_posts, name='all_posts'),
-    path('posts/<slug>', view=post_details, name='post_details'),
+    path('posts/<slug:slug>', view=post_details, name='post_details'),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,5 +8,5 @@ def index(request):
 def all_posts(request):
     return render(request=request, template_name='blog/all_posts.html')
 
-def post_details(request):
-    pass
+def post_details(request, slug):
+    return render(request=request, template_name='blog/post_detail.html')
