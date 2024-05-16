d5914cfb6f379675d760d513141f01a275d9d9d4
nipsalvin <amwaniki.am@gmail.com>
2024-05-16 19:12:14 +0300

    adding dummy data to the view file

--- test/views.py
+++ test/views.py
@@ -1,12 +1,9 @@
-from datetime import date
-from django.shortcuts import render
-
 posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Nips",
-        "date": date.today(),
+        "author": "Maximilian",
+        "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -26,8 +23,8 @@ posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Nips",
-        "date": date.today(),
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -47,8 +44,8 @@ posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Nips",
-        "date": date.today(),
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -66,15 +63,3 @@ posts = [
         """
     }
 ]
-
-
-def index(request):
-    return render(request=request, template_name='blog/index.html')
-
-
-def all_posts(request):
-    return render(request=request, template_name='blog/all_posts.html')
-
-
-def post_details(request, slug):
-    return render(request=request, template_name='blog/post_detail.html')
