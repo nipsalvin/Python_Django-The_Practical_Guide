59dc1067f1560b9a4754d5d3e007bab3ee9d04bf
nipsalvin <amwaniki.am@gmail.com>
2024-05-22 07:26:00 +0300

    adding more fields to `models`
    definging a `def __str__`

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50) 
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+    author = models.CharField(max_length=50, null=True)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.title} - ({self.rating})"
