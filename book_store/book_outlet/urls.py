f5502eb49f95541a7eae61302c1b46e8e5a46515
nipsalvin <amwaniki.am@gmail.com>
2024-05-24 07:14:47 +0300

    rendering details page

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:id>', views.book_detail),
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.http import Http404
 
 def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {'books':books})
+
+def book_detail(request, id):
+    # # Method 1
+    # book = get_object_or_404(Book, id)
+    # return render(request, 'book_outlet/book_detail.html', {'book':book})
+    # Method 2
+    try:
+        book = Book.objects.get(id=id)
+        return render(request, 'book_outlet/book_detail.html', {'book':book})
+    except:
+        raise Http404()
+    
