605aad4af1dd712d154d8caa6022c281634fdd3f
nipsalvin <amwaniki.am@gmail.com>
2024-05-30 07:18:09 +0300

    working with `OneToOne`

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author, Address
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,9 +3,22 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postcode = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.street} | {self.city}'
+    
+    class Meta:
+        verbose_name_plural = "Addresses"
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
