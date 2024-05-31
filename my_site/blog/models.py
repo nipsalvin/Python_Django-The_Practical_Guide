d71d92377bf9c5c4044cf0230fc185f6e8b65b73
nipsalvin <amwaniki.am@gmail.com>
2024-05-31 07:29:53 +0300

    `data` and `models` of blog

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, EmailValidator
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(validators=[EmailValidator()])
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", blank=True ,null=False, db_index=True, unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(3000)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
