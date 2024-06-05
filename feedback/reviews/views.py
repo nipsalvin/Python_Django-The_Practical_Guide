a56b70a8ba32454857b650bffad524ebad3e26a3
nipsalvin <amwaniki.am@gmail.com>
2024-06-05 07:57:53 +0300

    handling form submissions

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def review(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        context = {'username':username}
+        return render(request, 'reviews/thanks_you.html', context)
     return render(request, 'reviews/review.html')
