cf025370afcd56c2380176a46581865311742296
nipsalvin <amwaniki.am@gmail.com>
2024-06-06 19:02:15 +0300

    handling form submissions

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.review, name='index'),
+    path('thank_you/', views.thank_you, name='thank_you'),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
     if request.method == 'POST':
         username = request.POST.get('username')
-        context = {'username':username}
-        return render(request, 'reviews/thanks_you.html', context)
-    return render(request, 'reviews/review.html')
+        print(username)
+        if username == '':
+            has_error = True
+            return render(request, 'reviews/review.html', {'has_error': has_error})
+        return HttpResponseRedirect('/thank_you/')
+    return render(request, 'reviews/review.html', {'has_error': False})
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
