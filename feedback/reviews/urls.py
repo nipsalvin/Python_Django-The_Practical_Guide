ce9d9dfadfb442a6f1b4a843740071dee9658e0b
nipsalvin <amwaniki.am@gmail.com>
2024-06-07 07:23:47 +0300

    `Class Based  Views`

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review, name='index'),
+    # path('', views.review, name='index'),
+    path('', views.ReviewView.as_view(), name='index'),
     path('thank_you/', views.thank_you, name='thank_you'),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,32 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
+from django.views import View
 
 # Create your views here.
-def review(request):
-    # if request.method == 'POST':
-    #     username = request.POST.get('username')
-    #     print(username)
-    #     if username == '':
-    #         has_error = True
-    #         return render(request, 'reviews/review.html', {'has_error': has_error})
-    #     return HttpResponseRedirect('/thank_you/')
 
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {'form': form})
+    
+    def post(self, request):
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            # review = Review(
-            #     username=form.cleaned_data['username'],
-            #     review_text=form.cleaned_data['review_text'],
-            #     rating=form.cleaned_data['rating'],
-            # )
-            # review.save()
             form.save() #Since we are now using a ModelForm, we don't need to create an object, we can just save the form
             return HttpResponseRedirect('/thank_you/')
-    else:
-        form = ReviewForm()
-    return render(request, 'reviews/review.html', {'form': form})
+        return render(request, 'reviews/review.html', {'form': form})
+
+# def review(request):
+#     # if request.method == 'POST':
+#     #     username = request.POST.get('username')
+#     #     print(username)
+#     #     if username == '':
+#     #         has_error = True
+#     #         return render(request, 'reviews/review.html', {'has_error': has_error})
+#     #     return HttpResponseRedirect('/thank_you/')
+
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             # review = Review(
+#             #     username=form.cleaned_data['username'],
+#             #     review_text=form.cleaned_data['review_text'],
+#             #     rating=form.cleaned_data['rating'],
+#             # )
+#             # review.save()
+#             form.save() #Since we are now using a ModelForm, we don't need to create an object, we can just save the form
+#             return HttpResponseRedirect('/thank_you/')
+#     else:
+#         form = ReviewForm()
+#     return render(request, 'reviews/review.html', {'form': form})
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
