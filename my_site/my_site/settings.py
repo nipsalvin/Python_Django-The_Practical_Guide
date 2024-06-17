1c98d20739fa1b3404d92517613a5fe44cca3f00
nipsalvin <amwaniki.am@gmail.com>
2024-06-17 11:57:19 +0300

    Preparation to deploy on `AWS`

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -22,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEVELOPMENT', True)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    '127.0.0.1', 
+    'localhost',
+    os.getenv('APP_HOST')
+    ]
 
 
 # Application definition
