5a8c7daa308af3e44a3f4a2d18386d17bd222a43
nipsalvin <amwaniki.am@gmail.com>
2024-06-25 07:39:51 +0300

    setting up `PostGreSQL` using `AWS' RDB`

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT':'5432',
     }
 }
 
