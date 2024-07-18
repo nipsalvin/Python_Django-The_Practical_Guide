f2a715f911a5ccdfed31ce581f2f7ece62254a54
nipsalvin <amwaniki.am@gmail.com>
2024-07-18 06:39:11 +0300

    `DTL` Django Template Language

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title' : 'The first meetup'},
+        {'title' : 'The second meetup'},
+        {'title' : 'The third meetup'},
+    ]
+    # import ipdb; ipdb.set_trace()
+    return render(request, 'meetups/index.html', {
+        'meetups' : meetups,
+        'show_meetups' : False,
+    })
