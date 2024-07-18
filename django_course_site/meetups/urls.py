1c30946a36b00fbaaa6ab0f89dc1e377eb73fb88
nipsalvin <amwaniki.am@gmail.com>
2024-07-18 07:21:53 +0300

    adding details page

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('meetups/', view=views.index, name='index'),
+    path('meetups/<slug:meetup_slug>', view=views.meetup_details, name='meetup_details'),
 ]
