2b4aab7815afcfa0eccdd63c9f23171278c1bc28
nipsalvin <amwaniki.am@gmail.com>
2024-07-29 07:21:36 +0300

    configuring admin panel

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -4,4 +4,9 @@ from .models import Meetup
 
 # Register your models here.
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_filter = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Meetup, MeetupAdmin)
