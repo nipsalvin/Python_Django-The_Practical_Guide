e9254c8c8b9fa987255d9735efab3dfae69089b7
nipsalvin <amwaniki.am@gmail.com>
2024-07-29 07:04:14 +0300

    Adding `Image Upload`

--- django_course_site/meetups/models.py
+++ django_course_site/meetups/models.py
@@ -5,6 +5,7 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
