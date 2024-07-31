e4d3c79fee035ccb411db7ce775ace67a68a0107
nipsalvin <amwaniki.am@gmail.com>
2024-07-31 06:47:12 +0300

    `many-to-many Relationships`

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- django_course_site/meetups/models.py
+++ django_course_site/meetups/models.py
@@ -8,6 +8,12 @@ class Location(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.address}'
+    
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
@@ -15,6 +21,7 @@ class Meetup(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     def __str__(self):
         return self.title
